adbd9450658d3a173b449e8123bb7c1e9ef062d2
Joel Cameron-Piel <j.cameron-piel@protonmail.com>
2020-11-09 21:44:48 +0800

    Created index and script files

---
+++ script.js
@@ -0,0 +1,11 @@
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+
+function computerPlay() {
+  // your code here!
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
